0a5c7396fd2a2640ce0aba0161355bc96ff77bf9
Playdata <Playdata@DESKTOP-PTD66I7>
2021-08-29 00:27:12 +0900

    test

---
+++ Test/src/test3/Main.java
@@ -0,0 +1,18 @@
+package test3;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+
+	for (int n = sc.nextInt() ; n <= 9; n++) {
+		for(int i =1; i <=9; i++) {
+			
+			System.out.println(n+ " * " +i+ " = "+ n*i);
+		}break;
+	}
+	
+}
+}
---
+++ Test/src/test3/Main4.java
@@ -0,0 +1,28 @@
+package test3;
+
+import java.io.*;
+
+public class Main4 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= T; i++) {
+			String s = br.readLine();
+			String arr[] = s.split(" "); //공백제거를 위해split을 사용함
+			  int A = Integer.parseInt(arr[0]); 
+			  int B = Integer.parseInt(arr[1]);
+			
+			bw.write((A+B)+"\n");
+			
+		}
+		bw.flush(); //시간초과로 인해 for문 밖에다가 선언
+		bw.close();
+
+	}
+
+}
