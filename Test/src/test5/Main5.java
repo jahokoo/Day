4b73cf50c56c171a4452150ed179061fe56dc656
Playdata <Playdata@DESKTOP-PTD66I7>
2021-09-04 01:53:53 +0900

    1546

---
+++ Test/src/test5/Main5.java
@@ -0,0 +1,29 @@
+package test5;
+
+import java.util.Scanner;
+
+public class Main5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int score[] = new int[n];
+		double m = 0;
+		double sum=0;
+		double avg=0;
+		for (int i = 0; i < n; i++) {
+			score[i] = sc.nextInt();
+			if ( score[i] > m) {
+				m= score[i];
+			}
+		}
+			for(int i=0; i <n; i++) {
+				sum = score[i] / m *100;
+				avg +=sum;
+			}
+		
+			System.out.println(avg/n);
+			}	
+		}	
+
